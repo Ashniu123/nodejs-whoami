b6a9e1e55592ec9c0be49ddeae21e3e640fc97d7
Nisheet Sinvhal <nisheet1.sinvhal@gmail.com>
2018-08-13 16:29:34 +0000

    Added pm2 deployment steps

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name      : 'WHOAMI',
+    script    : 'server.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-34-230-31-235.compute-1.amazonaws.com',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Ashniu123/nodejs-whoami.git',
+      path : '/home/ubuntu/nodejs-whoami',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
